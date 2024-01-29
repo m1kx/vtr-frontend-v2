d1f437129f65ecf116a325bccaecba88010b9144
m1kx <mika.rottlaender@icloud.com>
2024-01-29 22:04:44 +0100

    disabled prerendering

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,8 @@ export default defineNuxtConfig({
   modules: ["@nuxt/ui"],
   devtools: { enabled: true },
   ssr: false,
+  routeRules: {
+    '*': { prerender: false },
+    '/auth/*': { prerender: false }
+  }
 });
