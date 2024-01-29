016ef8931255fc5d1f1ed29e56fefa3671551620
m1kx <mika.rottlaender@icloud.com>
2024-01-29 22:08:08 +0100

    updated nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ["@nuxt/ui"],
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   ssr: false,
   routeRules: {
-    '*': { prerender: false },
-    '/auth/*': { prerender: false }
+    '*': { prerender: false, ssr: false },
+    '/auth/*': { prerender: false, ssr: false }
   }
 });
